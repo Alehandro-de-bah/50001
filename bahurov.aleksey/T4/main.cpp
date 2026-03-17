83628966beed7f2824a5eb9d9880f0988405936d
Aleksey Bahurov <223736665+Alehandro-de-bah@users.noreply.github.com>
2026-03-17 14:36:59 +0300

    Special commit for acceptance test

--- bahurov.aleksey/T4/main.cpp
+++ bahurov.aleksey/T4/main.cpp
@@ -9,15 +9,18 @@ int main()
         std::vector<std::unique_ptr<Shape>> figures;
 
         auto compositeFigure = std::make_unique<CompositeShape>();
-        compositeFigure->addShape(std::make_unique<Rectangle>(Point(1.0, 2.0), Point(5.0, 7.0))); // Добавляем прямоугольник
-        compositeFigure->addShape(std::make_unique<Rectangle>(Point(0.0, 1.5), Point(3.0, 9.0))); // Добавляем прямоугольник
-        compositeFigure->addShape(std::make_unique<Ellipse>(Point(2.5, 3.5), 7.0, 4.0)); // Добавляем эллипс
+        compositeFigure->addShape(std::make_unique<Rectangle>(Point(1.0, 2.0), Point(5.0, 7.0)));
+        compositeFigure->addShape(std::make_unique<Rectangle>(Point(0.0, 1.5), Point(3.0, 9.0)));
+        compositeFigure->addShape(std::make_unique<Ellipse>(Point(2.5, 3.5), 7.0, 4.0));
 
-        figures.push_back(std::move(compositeFigure)); // Добавляем составную фигуру
-        figures.push_back(std::make_unique<Rectangle>(Point(0.4, 1.1), Point(4.3, 8.9))); // Добавляем прямоугольник
-        figures.push_back(std::make_unique<Rectangle>(Point(1.3, 3.6), Point(6.6, 6.7))); // Добавляем прямоугольник
-        figures.push_back(std::make_unique<Ellipse>(Point(2.45, 4.15), 7.24, 3.14)); // Добавляем эллипс
-        figures.push_back(std::make_unique<Ellipse>(Point(2.8, 5.7), 8.4, 5.1)); // Добавляем эллипс
+        // Добавляем составную фигуру
+        figures.push_back(std::move(compositeFigure));
+
+        // Добавляем простые фигуры (прямоугольники и эллипсы)
+        figures.push_back(std::make_unique<Rectangle>(Point(0.4, 1.1), Point(4.3, 8.9)));
+        figures.push_back(std::make_unique<Rectangle>(Point(1.3, 3.6), Point(6.6, 6.7)));
+        figures.push_back(std::make_unique<Ellipse>(Point(2.45, 4.15), 7.24, 3.14));
+        figures.push_back(std::make_unique<Ellipse>(Point(2.8, 5.7), 8.4, 5.1));
 
         std::cout << "===== Information about figures before 2x increase =====" << std::endl;
         for (size_t i = 0; i < figures.size(); i++)
@@ -27,7 +30,7 @@ int main()
 
         for (size_t i = 0; i < figures.size(); i++)
         {
-            figures[i]->scale(2);
+            figures[i]->scale(2.0);
         }
 
         std::cout << "===== Information about figures after 2x increase =====" << std::endl;
