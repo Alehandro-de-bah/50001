67401570af9a1f8b762925c68a99d15e1f0a66a9
Aleksey Bahurov <223736665+Alehandro-de-bah@users.noreply.github.com>
2026-04-28 21:18:16 +0300

    Initial commit for T3

---
+++ bahurov.aleksey/T3/polygon.cpp
@@ -0,0 +1,39 @@
+#include "polygon.h"
+
+namespace bahurov
+{
+    // Перегрузка оператора ввода для структуры Point
+    std::istream& operator>>(std::istream& in, Polygon& polygon)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry)
+        {
+            return in;
+        }
+
+        size_t vertexes = 0;
+        in >> vertexes;
+
+        if (!in || vertexes < 3)
+        {
+            in.setstate(std::ios::failbit);
+            return in;
+        }
+        
+        Polygon input;
+        std::generate_n(std::back_inserter(input.points), vertexes,
+                [&in]()
+                {
+                    Point point;
+                    in >> DelimetrIO{ ' ' } >> point;
+                    return point;
+                }
+        );
+
+        if (in)
+        {
+            polygon = std::move(input);
+        }
+        return in;
+    }
+}
---
+++ bahurov.aleksey/T3/polygon.h
@@ -0,0 +1,19 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+
+#include <vector>
+#include <algorithm>
+#include "point.h"
+
+namespace bahurov
+{
+    struct Polygon
+    {
+        std::vector<Point> points;
+    };
+
+    // Перегрузка оператора ввода для структуры Polygon
+    std::istream& operator>>(std::istream& in, Polygon& polygon);
+}
+
+#endif
